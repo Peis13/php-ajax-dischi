f974ed6cda9d8a39fcfedf23c793e40392489743
Nicolò <nico.paganelli91@gmail.com>
2020-07-16 16:52:30 +0200

    Init File  sass e laravelMix

---
+++ dist/index-php.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Musica</title>
+    <link rel="stylesheet" href="css/entry.css">
+  </head>
+  <body>
+    
+  </body>
+</html>
